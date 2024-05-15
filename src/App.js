7d7f5469c5f8ce3d769a331092e3c7498a01dfda
skywalker-89 <julforschool1@gmail.com>
2024-05-15 10:18:30 +0700

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import "./App.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  console.log("Show employee");
+  const [role, setRole] = useState("No Role");
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello there</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log("This is inside the return.")}
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Caleb" role="Intern" />
+          <Employee name="Abby" role={role} />
+          <Employee name="John" />
+        </>
+      ) : (
+        <p>You can not see the employee.</p>
+      )}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,10 @@
+function Employee(props) {
+  return (
+    <>
+      <h3>Employee {props.name}</h3>
+      <p>{props.role ? props.role : "No Role"}</p>
+    </>
+  );
+}
+
+export default Employee;
